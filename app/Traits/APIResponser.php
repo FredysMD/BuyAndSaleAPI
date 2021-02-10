ea9d58c9cb7c1fbcfc483311bcdadcb0f4459a1c
Fredys Márquez Duque <fmarquezduque@gmail.com>
2021-02-10 14:36:08 -0500

    Paginate of data

--- app/Traits/APIResponser.php
+++ app/Traits/APIResponser.php
@@ -3,7 +3,9 @@
 namespace App\Traits;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 trait APIResponser
 {
@@ -26,6 +28,7 @@ trait APIResponser
 		
 		$collection = $this->filterData($collection, $transformer);
 		$collection = $this->sortData($collection, $transformer);
+		$collection = $this->paginate($collection);
 		$collection = $this->transformData($collection, $transformer);
 		 	
 		return $this->successResponse(['data' => $collection], $code);
@@ -74,5 +77,30 @@ trait APIResponser
 		}
 
 		return $collection;
+	}
+
+	protected function paginate(Collection $collection)
+	{	
+		$rules = [
+			'per_page' => 'integer|min:2|max:50'
+		];
+
+
+		Validator::validate(request()->all(), $rules);
+
+		$page = LengthAwarePaginator::resolveCurrentPage();
+		$perPage = 15;
+
+		if(request()->has('per_page'))
+			$perPage = (int) request()->per_page;
+
+		$result = $collection->slice(($page - 1)* $perPage, $perPage);
+
+		$paginator= new LengthAwarePaginator($result, $collection->count(), $perPage, $page, [
+			'path' => LengthAwarePaginator::resolveCurrentPath(),
+		]);
+		
+		$paginator->append(request()->all());
+		return $paginator;
 	}
 }
